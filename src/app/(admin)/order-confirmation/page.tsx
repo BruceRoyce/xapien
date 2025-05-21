952f91dae192fa31dfb491196252eef6dde417e7
Bruce Royce <brucerroyce@gmail.com>
2025-05-21 04:20:44 +0100

    fix: reset detect topup

--- src/app/(admin)/order-confirmation/page.tsx
+++ src/app/(admin)/order-confirmation/page.tsx
@@ -9,40 +9,38 @@ import GeneralCard from "@/components/cards/GeneralCard";
 import useStore from "@/store/useStore";
 
 export default function OrderConfirmationPage() {
-	const searchParams = useSearchParams();
+  const searchParams = useSearchParams();
 
-	const orderId = searchParams.get("orderId");
-	const planId = searchParams.get("planId");
-	const accountId = searchParams.get("accountId");
+  const orderId = searchParams.get("orderId");
+  const planId = searchParams.get("planId");
+  const accountId = searchParams.get("accountId");
 
-	const { setSelectedPlan, setSelectedClient } = useStore();
+  const { setSelectedPlan, setSelectedClient, setIsTopup } = useStore();
 
-	useEffect(() => {
-		setSelectedPlan(null);
-		setSelectedClient(null);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+  useEffect(() => {
+    setSelectedPlan(null);
+    setSelectedClient(null);
+    setIsTopup(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	return (
-		<GeneralCard
-			title="Order Confirmation"
-			className="text-center"
-		>
-			<>
-				<div>
-					<p>Order ID: {orderId}</p>
-					<p>Plan ID: {planId}</p>
-					<p>Account ID: {accountId}</p>
-				</div>
-				<Link href="/dashboard">
-					<Button
-						variant="secondary"
-						label="Go back to dashboard"
-						child={<ArrowBackward />}
-						childrenPosition="left"
-					/>
-				</Link>
-			</>
-		</GeneralCard>
-	);
+  return (
+    <GeneralCard title="Order Confirmation" className="text-center">
+      <>
+        <div>
+          <p>Order ID: {orderId}</p>
+          <p>Plan ID: {planId}</p>
+          <p>Account ID: {accountId}</p>
+        </div>
+        <Link href="/dashboard">
+          <Button
+            variant="secondary"
+            label="Go back to dashboard"
+            child={<ArrowBackward />}
+            childrenPosition="left"
+          />
+        </Link>
+      </>
+    </GeneralCard>
+  );
 }
